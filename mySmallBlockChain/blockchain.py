04f21141032940e90c019822ee2ccae9b0253e0b
pengjing <pjinger96@bupt.edu.cn>
2018-07-25 17:36:22 +0800

    完成 snakecoin的编写

---
+++ mySmallBlockChain/blockchain.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+# blockchain.py
+
+import hashlib
+import json
+from time import time
+
+
+class Blockchain(object):
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+
+    def new_block(self, proof, previous_hash=None):
+        """
+        创建一个新的区块到区块链中
+        :param proof: <int> 由工作证明算法生成的证明
+        :param previous_hash: (Optional) <str> 前一个区块的 hash 值
+        :return: <dict> 新区块
+        """
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+
+        # 重置当前交易记录
+        self.current_transactions = []
+
+        self.chain.append(block)
+        return block
+
+    def new_transactions(self, sender, recipient, amount):
+        """
+        创建一笔新的交易到下一个被挖掘的区块中
+        :param sender: <str> 发送人的地址
+        :param recipient: <str> 接收人的地址
+        :param amount: <int> 金额
+        :return: <int> 持有本次交易的区块索引
+        """
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount,
+        })
+
+        return self.last_block['index'] + 1
+
+    @property
+    def last_block(self):
+        return self.chain[-1]
+
+    @staticmethod
+    def hash(block):
+        """
+        给一个区块生成 SHA-256 值
+        :param block: <dict> Block
+        :return: <str>
+        """
+        # 我们必须确保这个字典（区块）是经过排序的，否则我们将会得到不一致的散列
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
+
+    def proof_of_work(self, last_proof):
+        """
+        Simple Proof of Work Algorithm:
+         - Find a number p' such that hash(pp') contains leading 4 zeroes, where p is the previous p'
+         - p is the previous proof, and p' is the new proof
+        :param last_proof: <int>
+        :return: <int>
+        """
+
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+
+        return proof
+
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        """
+        Validates the Proof: Does hash(last_proof, proof) contain 4 leading zeroes?
+        :param last_proof: <int> Previous Proof
+        :param proof: <int> Current Proof
+        :return: <bool> True if correct, False if not.
+        """
+
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
+
+'''
+block example:
+
+block = {
+    'index': 1,
+    'timestamp': 1506057125.900785,
+    'transactions': [
+        {
+            'sender': "8527147fe1f5426f9dd545de4b27ee00",
+            'recipient': "a77f5cdfa2934df3954a5c7c7da5df1f",
+            'amount': 5,
+        }
+    ],
+    'proof': 324984774000,
+    'previous_hash': "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824"
+}
+'''
